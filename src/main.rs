ed0ceccaa2b28a0414b6fb29ec3bae82194fb8e1
Keunhong Lee <dlrmsghd@gmail.com>
2017-01-30 14:13:52 +0900

    Practice
    Signed-off-by: Keunhong Lee <dlrmsghd@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::sync;
+use std::thread;
+
 fn main() {
-	println!("Hello");
+	let mut children = vec![];
+	let counter = sync::Arc::new(sync::RwLock::new(0usize));
+	
+	for thread_id in 0..4 {
+		let val = counter.clone();
+		children.push(thread::spawn(move || {
+					println!("Hello thread {}", thread_id);
+					{
+						println!("Current value (tid:{}), {}", thread_id, *val.read().unwrap());
+					}
+					{
+						let mut write = val.write().unwrap();
+						*write += thread_id;
+						println!("Updated value (tid:{}) {}", thread_id, *write);
+					}
+		}));
+	}
+	
+	for thread in children {
+		let result = thread.join();
+		if let Err(err) = result {
+			println!("Error occurred! {:?}", err);
+		}
+	}
 }
